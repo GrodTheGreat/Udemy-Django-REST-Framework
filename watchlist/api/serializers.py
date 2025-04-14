3a0ead9bcd17684aab3146e023c00b404f384036
Gerard Causse <gcausse@epri.com>
2025-04-14 15:07:58 -0400

    Complete Serializers  - POST, PUT, DELETE Request lesson

--- watchlist/api/serializers.py
+++ watchlist/api/serializers.py
@@ -1,4 +1,7 @@
+from typing import Any, Mapping
 from rest_framework import serializers
+
+from ..models import Movie
 
 
 class MovieSerializer(serializers.Serializer):
@@ -6,3 +9,24 @@ class MovieSerializer(serializers.Serializer):
     name = serializers.CharField()
     description = serializers.CharField()
     active = serializers.BooleanField()
+
+    def create(self, validated_data: Mapping[str, Any]) -> Movie:
+        return Movie.objects.create(**validated_data)
+
+    def update(
+        self,
+        instance: Movie,
+        validated_data: Mapping[str, Any],
+    ) -> Movie:
+        instance.name = validated_data.get(key="name", default=instance.name)
+        instance.description = validated_data.get(
+            key="description",
+            default=instance.description,
+        )
+        instance.active = validated_data.get(
+            key="active",
+            default=instance.active,
+        )
+        instance.save()
+
+        return instance
--- watchlist/api/views.py
+++ watchlist/api/views.py
@@ -1,22 +1,48 @@
 from django.http import HttpRequest
 from rest_framework.decorators import api_view
+from rest_framework.request import Request
 from rest_framework.response import Response
 
 from watchlist.api.serializers import MovieSerializer
 from watchlist.models import Movie
 
 
-@api_view()
-def movie_list(request: HttpRequest) -> Response:
-    movies = Movie.objects.all()
-    serializer = MovieSerializer(movies, many=True)
+@api_view(http_method_names=["GET", "POST"])
+def movie_list(request: HttpRequest | Request) -> Response:
+    if request.method == "GET":
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)
 
-    return Response(data=serializer.data)
+        return Response(data=serializer.data)
+
+    if request.method == "POST":
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
 
 
-@api_view()
-def movie_details(request: HttpRequest, pk: int) -> Response:
-    movie = Movie.objects.get(pk=pk)
-    serializer = MovieSerializer(movie)
+@api_view(http_method_names=["GET", "PUT", "DELETE"])
+def movie_details(request: HttpRequest | Request, pk: int) -> Response:
+    if request.method == "GET":
+        movie = Movie.objects.get(pk=pk)
+        serializer = MovieSerializer(movie)
 
-    return Response(data=serializer.data)
+        return Response(data=serializer.data)
+
+    if request.method == "PUT":
+        movie = Movie.objects.get(pk=pk)
+        serializer = MovieSerializer(movie, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+    if request.method == "DELETE":
+        movie = Movie.objects.get(pk=pk)
+        movie.delete()
+
+        return Response()
