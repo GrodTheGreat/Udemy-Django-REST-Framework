9710567ddd9dc2a3de9a7bff83ea876243293421
Gerard Causse <gcausse@epri.com>
2025-04-14 15:47:28 -0400

    Complete Validation lesson

--- watchlist/api/serializers.py
+++ watchlist/api/serializers.py
@@ -4,9 +4,16 @@ from rest_framework import serializers
 from ..models import Movie
 
 
+def name_length(value: str) -> str:
+    if len(value) < 2:
+        raise serializers.ValidationError("Name is too short!")
+
+    return value
+
+
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField()
+    name = serializers.CharField(validators=[name_length])
     description = serializers.CharField()
     active = serializers.BooleanField()
 
@@ -27,3 +34,18 @@ class MovieSerializer(serializers.Serializer):
         instance.save()
 
         return instance
+
+    # You might be able to use Validators for this instead
+    # def validate_name(self, value: str) -> str:
+    #     if len(value) < 2:
+    #         raise serializers.ValidationError("Name is too short!")
+
+    #     return value
+
+    def validate(self, data: Mapping[str, Any]) -> Mapping[str, Any]:
+        if data["name"] == data["description"]:
+            raise serializers.ValidationError(
+                "Name and Description must be different!"
+            )
+
+        return data
