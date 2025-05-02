065af73908413651e5fccaef2717b7feb01545ce
Gerard Causse <grod.causse@gmail.com>
2025-05-01 20:04:41 -0400

    Complete Token Authentication - Part 3 (Login) lesson

--- watchmate/settings.py
+++ watchmate/settings.py
@@ -40,8 +40,9 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    "watchlist",
     "rest_framework.authtoken",
+    "accounts",
+    "watchlist",
 ]
 
 MIDDLEWARE = [
--- watchmate/urls.py
+++ watchmate/urls.py
@@ -21,5 +21,6 @@ from django.urls import include, path
 urlpatterns = [
     path(route="admin/", view=admin.site.urls),
     path(route="watchlist/", view=include("watchlist.api.urls")),
+    path(route="accounts/", view=include("accounts.api.urls")),
     # path(route="api-auth/", view=include("rest_framework.urls")),
 ]
